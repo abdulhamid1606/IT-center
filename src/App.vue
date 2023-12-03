49fa42ca27ac9959f2aaff6dc58099f98a0f6890
Abdulhamid1606 <abdulhamidturgunov1606@gmail.com>
2023-12-03 12:18:38 +0500

    Salomar

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+import mainS from './assets/scss/main.scss'
+</script>
+
+<template>
+    <header>
+        <div>
+            <Header />
+            <Footer />
+            <Main />
+        </div>
+    </header>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div>
+        <h1>Salom</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'App',
+    components: {
+        Headers,
+    }
+}
+</script>
+<style lang="">
+    
+</style>
